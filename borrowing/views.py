39341eb5ec77af251174ecfc52814deec9082993
Andriy Ishchenko <136181910+AndriiIshchenko@users.noreply.github.com>
2024-12-20 19:02:44 +0200

    feat: add base serializer/view for borrowing

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingSerializer
+
+
+class BorrowingViewSet(
+    GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.RetrieveModelMixin,
+):
+    serializer_class = BorrowingSerializer
+    queryset = Borrowing.objects.all()
+    permission_classes = [IsAuthenticated,]
