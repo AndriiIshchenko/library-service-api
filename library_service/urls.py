d4b4d8c60083ae7a1d62c40cfada490c56d42176
Andriy Ishchenko <136181910+AndriiIshchenko@users.noreply.github.com>
2024-12-20 18:17:22 +0200

    feat: inicialize borrowing app

--- library_service/urls.py
+++ library_service/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/books/", include("books.urls", namespace="books")),
+    path("api/borrowing/", include("borrowing.urls", namespace="borrowing")),
     path("__debug__/", include("debug_toolbar.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
