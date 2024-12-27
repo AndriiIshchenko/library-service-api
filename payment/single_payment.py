51cb8d4e39f58978ce55520f3aa50e3a3e6e95ab
Andriy Ishchenko <136181910+AndriiIshchenko@users.noreply.github.com>
2024-12-27 19:01:28 +0200

    feat: update success/cancel urls in session crearion

--- payment/single_payment.py
+++ payment/single_payment.py
@@ -1,16 +1,20 @@
 import stripe
 
 from django.conf import settings
+from django.urls import reverse
+from stripe.checkout._session import Session
 
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-def create_payment_session(borrowing):
+def create_payment_session(borrowing, request) -> Session:
     """
     Create a payment session for a borrowing using Stripe Checkout
     """
     try:
+        success_url = request.build_absolute_uri(reverse("payment:payment-success"))
+        cancel_url = request.build_absolute_uri(reverse("payment:payment-cancel"))
         session = stripe.checkout.Session.create(
             payment_method_types=["card"],
             line_items=[
@@ -20,16 +24,14 @@ def create_payment_session(borrowing):
                         "product_data": {
                             "name": f"Payment for {borrowing.id}",
                         },
-                        "unit_amount": int(
-                            borrowing.money_to_pay * 100
-                        ),
+                        "unit_amount": int(borrowing.money_to_pay * 100),
                     },
                     "quantity": 1,
                 }
             ],
             mode="payment",
-            success_url="https://your-domain.com/success",
-            cancel_url="https://your-domain.com/cancel",
+            success_url=success_url + "?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=cancel_url,
         )
         return session
     except Exception as e:
